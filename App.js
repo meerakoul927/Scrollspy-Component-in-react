7782dcd5291fb6592c63fc6791c9fefcc89cf1f5
koulmeera927@gmail.com <koulmeera927@gmail.com>
2025-02-18 08:00:12 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+export default App;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
